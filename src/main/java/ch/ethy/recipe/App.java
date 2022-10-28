960a785f41f8fe150590318926bf1d930dfcf13a
Fabian Gut <fabian.gut@ethy.ch>
2022-10-28 09:50:22 +0200

    Setup Maven

---
+++ src/main/java/ch/ethy/recipe/App.java
@@ -0,0 +1,20 @@
+package ch.ethy.recipe;
+
+import io.undertow.*;
+import io.undertow.util.*;
+
+/**
+ * Hello world!
+ */
+public class App {
+  public static void main(String[] args) {
+    Undertow server = Undertow.builder()
+        .addHttpListener(8080, "localhost")
+        .setHandler(exchange -> {
+          exchange.getResponseHeaders().put(Headers.CONTENT_TYPE, "text/plain");
+          exchange.getResponseSender().send("Hello World!");
+        }).build();
+
+    server.start();
+  }
+}
