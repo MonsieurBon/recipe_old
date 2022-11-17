d41f594052762a287b8c737be4d78729b2db5e29
Fabian Gut <fabian.gut@ethy.ch>
2022-11-17 13:30:45 +0100

    Basic handler setup

--- src/main/java/ch/ethy/recipe/App.java
+++ src/main/java/ch/ethy/recipe/App.java
@@ -1,20 +1,48 @@
 package ch.ethy.recipe;
 
 import io.undertow.*;
-import io.undertow.util.*;
+import io.undertow.server.*;
+import io.undertow.server.handlers.*;
+import io.undertow.server.handlers.error.*;
+import io.undertow.server.handlers.resource.*;
+import java.net.*;
+import java.nio.file.*;
 
 public class App {
 
-  public static void main(String[] args) {
+  public static void main(String[] args) throws Exception {
+    App app = new App();
+    app.start();
+  }
+
+  private void start() {
     Undertow server = Undertow
       .builder()
       .addHttpListener(8080, "localhost")
-      .setHandler(exchange -> {
-        exchange.getResponseHeaders().put(Headers.CONTENT_TYPE, "text/plain");
-        exchange.getResponseSender().send("Hello World!");
-      })
+      .setHandler(getHandler())
       .build();
 
     server.start();
+  }
+
+  private HttpHandler getHandler() {
+    URL indexResource = App.class.getResource("public/index.html");
+    HttpHandler fileNotFoundHandler = ResponseCodeHandler.HANDLE_404;
+    if (indexResource != null) {
+      try {
+        Path index = Path.of(indexResource.toURI());
+        fileNotFoundHandler =
+          new FileErrorPageHandler(ResponseCodeHandler.HANDLE_200, index, 200);
+      } catch (URISyntaxException e) {
+        System.out.println("Failed to create fallback resource handler");
+      }
+    }
+
+    ResourceHandler resourceHandler = new ResourceHandler(
+      new ClassPathResourceManager(App.class.getClassLoader(), "public"),
+      fileNotFoundHandler
+    );
+
+    return Handlers.path(resourceHandler);
   }
 }
