1b8a35f0c7a36e8ebaeedfe719a75f21bdacc70c
Fabian Gut <fabian.gut@ethy.ch>
2024-01-30 17:36:46 +0100

    Generated spring boot project

---
+++ src/test/java/ch/ethy/recipe/RecipeApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.ethy.recipe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
